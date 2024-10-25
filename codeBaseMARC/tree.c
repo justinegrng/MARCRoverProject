231c331cccd9d703adec6e0bb4de4d2e075636dd
Mélia <melia.tanguy@gmail.com>
2024-10-25 12:28:04 +0200

    création structures : noeuds et abres

---
+++ codeBaseMARC/tree.c
@@ -0,0 +1,6 @@
+//
+// Created by melia on 25/10/2024.
+//
+#include <stdlib.h>
+#include <stdio.h>
+#include "tree.h"
---
+++ codeBaseMARC/tree.h
@@ -0,0 +1,13 @@
+//
+// Created by melia on 25/10/2024.
+//
+
+#ifndef UNTITLED1_TREE_H
+#define UNTITLED1_TREE_H
+#include "node.h"
+typedef struct s_tree
+{
+    p_node root;
+} t_tree, *p_tree;
+
+#endif //UNTITLED1_TREE_H
