c898ff243c7542ef5ecb275b9b265ef9340e7acc
justinegrng <jugarnung@gmail.com>
2024-11-13 12:26:13 +0100

    correction findPathToMinLeaf find the right path now

--- codeBaseMARC/tree.c
+++ codeBaseMARC/tree.c
@@ -29,28 +29,50 @@ void freeTree(Node* root) {
 //Trouver la feuille minimum
 int findMinLeaf(Node* root) {
     if (root == NULL) return INT_MAX;
-    if (root->left == NULL && root->middle == NULL && root->right == NULL) return root->value;
-
+    if (root->left == NULL && root->middle == NULL && root->right == NULL) {
+        return root->value;
+    }
     int leftMin = findMinLeaf(root->left);
     int middleMin = findMinLeaf(root->middle);
     int rightMin = findMinLeaf(root->right);
 
-    return (leftMin < middleMin) ? ((leftMin < rightMin) ? leftMin : rightMin) : ((middleMin < rightMin) ? middleMin : rightMin);
+    return (leftMin < middleMin) ?
+           ((leftMin < rightMin) ? leftMin : rightMin) :
+           ((middleMin < rightMin) ? middleMin : rightMin);
 }
 
 
 //Trouver le chemin vers la feuille minimum
 int findPathToMinLeaf(Node* root, int* path, int pathLen) {
-    if (root == NULL) return 0;
+    if (root == NULL) {
+        return 0;
+    }
     path[pathLen] = root->value;
     pathLen++;
 
-    if (root->left == NULL && root->middle == NULL && root->right == NULL) return pathLen;
+    if (root->left == NULL && root->middle == NULL && root->right == NULL){
+        return pathLen;
+    }
 
-    int leftPathLen = findPathToMinLeaf(root->left, path, pathLen);
-    int middlePathLen = findPathToMinLeaf(root->middle, path, pathLen);
-    int rightPathLen = findPathToMinLeaf(root->right, path, pathLen);
+    int leftPath[100], middlePath[100], rightPath[100];
+    int leftPathLen = findPathToMinLeaf(root->left, leftPath, 0);
+    int middlePathLen = findPathToMinLeaf(root->middle, middlePath, 0);
+    int rightPathLen = findPathToMinLeaf(root->right, rightPath, 0);
+    int minPathLen = leftPathLen;
+    int* minPath = leftPath;
 
-    int minPathLen = (leftPathLen < middlePathLen) ? ((leftPathLen < rightPathLen) ? leftPathLen : rightPathLen) : ((middlePathLen < rightPathLen) ? middlePathLen : rightPathLen);
-    return minPathLen;
+    if (middlePathLen < minPathLen) {
+        minPathLen = middlePathLen;
+        minPath = middlePath;
+    }
+    if (rightPathLen < minPathLen) {
+        minPathLen = rightPathLen;
+        minPath = rightPath;
+    }
+
+    for (int i = 0; i < minPathLen; i++) {
+        path[pathLen + i] = minPath[i];
+    }
+
+    return pathLen + minPathLen;
 }
