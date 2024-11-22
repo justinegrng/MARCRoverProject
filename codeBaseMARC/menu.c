136d83f91ea21cf5a729ee34b18667359dc213bb
justinegrng <jugarnung@gmail.com>
2024-11-22 13:32:59 +0100

    add menu

---
+++ codeBaseMARC/menu.c
@@ -0,0 +1,28 @@
+//
+// Created by jugar on 22/11/2024.
+//
+
+#include "menu.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * @brief Function to display the main menu
+ * @param none
+ * @return none
+ */
+
+void displayMenu() {
+    printf("1. Afficher l'arbre\n");
+    printf("2. Afficher la feuille minimum\n");
+    printf("3. Afficher le chemin vers la feuille minimum\n");
+    printf("4. Sortir\n");
+}
+
+/**
+ * @brief Function to get the user's choice
+ * @param none
+ * @return the user's choice
+ */
+
---
+++ codeBaseMARC/menu.h
@@ -0,0 +1,10 @@
+//
+// Created by jugar on 22/11/2024.
+//
+
+#ifndef MARCROVER_MENU_H
+#define MARCROVER_MENU_H
+
+#endif //MARCROVER_MENU_H
+
+void displayMenu();
